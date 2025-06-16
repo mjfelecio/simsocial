fdf93eb96434c2d7d13e1e5fabdef78820e055d2
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-16 18:16:14 +0800

    feat: Define route for creating user profile

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   get "/users/all", to: "users#all", as: :all_users
   get "/users/:id/profile", to: "users#profile", as: :user_profile
   get "/users/:id/profile/new", to: "users#new_profile", as: :new_user_profile
+  patch "/users/:id/profile", to: "users#create_profile", as: :create_user_profile
 end
