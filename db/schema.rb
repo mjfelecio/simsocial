b95f9998a90682dacfd62acd7880f2089f815f05
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-16 15:02:10 +0800

    feat: Scaffold a post

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_16_070024) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  add_foreign_key "posts", "users"
 end
