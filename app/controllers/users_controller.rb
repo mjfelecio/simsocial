ed9513e5fb58b423888e121723659b9f0a17ac0e
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-16 18:16:41 +0800

    feat: Implement create_profile action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def create_profile
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_path, notice: "User Profile was successfully created." }
+        format.json { render :profile, status: :created, location: @user }
+      else
+        format.html { render :new_profile, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :name, :profile_picture)
