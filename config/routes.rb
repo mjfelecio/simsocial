6889a354a7a73a43eddd1beff369653bcb4b3676
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-17 19:47:09 +0800

    feat: Simplify user routes by nesting them inside

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :posts
+    get "/", to: "users#index", as: :users
+    get "/profile", to: "users#profile", as: :user_profile
+    get "/profile/new", to: "users#new_profile", as: :new_user_profile
+    get "/profile/edit", to: "users#edit_profile", as: :edit_user_profile
+    patch "/profile", to: "users#update_profile", as: :update_profile
   end
-
-  get "/users", to: "users#index", as: :users
-  get "/users/:id/profile", to: "users#profile", as: :user_profile
-  get "/users/:id/profile/new", to: "users#new_profile", as: :new_user_profile
-  get "/users/:id/profile/edit", to: "users#edit_profile", as: :edit_user_profile
-  patch "/users/:id/profile", to: "users#update_profile", as: :update_profile
 end
