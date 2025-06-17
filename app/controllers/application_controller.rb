654a669227cccd76c37c9954017d7d1d9e2f1774
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-17 19:26:33 +0800

    feat: Redirect user to create profile after sign in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Redirects the user to create profile after login or signup if they have no username
+  def after_sign_in_path_for(resource)
+    if current_user.username.blank?
+      new_user_profile_path(current_user)
+    else
+      root_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    if current_user.username.blank?
+      new_user_profile_path(current_user)
+    else
+      root_path
+    end
+  end
 end
