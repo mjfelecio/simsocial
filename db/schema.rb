1fd6659188126f96fe0283b48cad804a543e479f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-19 11:00:39 +0800

    feat: Generate Like polymorphic model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_16_072332) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_025958) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "likable_type", null: false
+    t.integer "likable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["likable_type", "likable_id"], name: "index_likes_on_likable"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -35,5 +45,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_16_072332) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
