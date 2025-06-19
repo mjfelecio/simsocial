b90aa8739a9e80ba284e2777a5dfdcc9e286a36b
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-19 15:29:39 +0800

    feat: Establish associations with comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likable, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :user_id, presence: true
 end
