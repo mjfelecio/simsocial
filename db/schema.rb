912aa85132d5b97891bf4d89792e64d221d3ef9a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-19 15:28:24 +0800

    feat: Generate comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_025958) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_072713) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "likable_type", null: false
@@ -45,6 +55,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_025958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
