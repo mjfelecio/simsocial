1f83f044880c7e616d39a4a3eb6907a6e54fcc4f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-22 18:01:26 +0800

    feat: Display followed and own post if signed in and display all posts if not

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    if user_signed_in?
+      # Posts from current user and people they follow
+      following_ids = current_user.following.pluck(:id)
+      @posts = Post.where(user_id: [ current_user.id ] + following_ids)
+                  .includes(:user, :comments, :likes)
+                  .order(created_at: :desc)
+    else
+      @posts = Post.all
+    end
   end
 end
