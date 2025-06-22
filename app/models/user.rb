0d16fa376b89d63f38838b76dc15a4894209e89d
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-22 17:29:23 +0800

    feat: Establish following associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,25 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # Following associations
+  has_many :active_follows, class_name: "Follow",
+           foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_follows, class_name: "Follow",
+           foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
