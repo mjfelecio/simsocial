7f065c3bd4a4dd98e591092b27da0dfd9ddd67ac
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-22 17:25:05 +0800

    feat: Generate follow model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_072713) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_22_092249) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_072713) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -57,6 +67,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_072713) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
