e6576eafd0e3b18ba293a36f400389bbb3b49a2a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-22 18:20:20 +0800

    feat: Adjust toast durations

--- config/initializers/rails_toastify.rb
+++ config/initializers/rails_toastify.rb
@@ -2,8 +2,8 @@ RailsToastify.setup do |configuration|
   configuration.position = "toast-container-top-right"
   configuration.notice_animation = "slide" # bounce, slide, flip, zoom
   configuration.alert_animation = "slide" # bounce, slide, flip, zoom
-  configuration.notice_duration = 3000
-  configuration.alert_duration = 3000
+  configuration.notice_duration = 2000
+  configuration.alert_duration = 2000
   configuration.notice_theme = "light" # light, dark
   configuration.alert_theme = "light" # light, dark
   configuration.notice_type = "success" # default, success, warning, error, info
